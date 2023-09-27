f5b8330be115b00af5b0e9268c81a353d83db973
daniel calado <danielcalado159@gmail.com>
2023-09-27 13:43:04 -0300

    envio projeto Selenium

---
+++ src/main/java/com/br/ifpe/projetoSelenium/App.java
@@ -0,0 +1,8 @@
+package com.br.ifpe.projetoSelenium;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ src/test/java/com/br/ifpe/projetoSelenium/AppTest.java
@@ -0,0 +1,13 @@
+package com.br.ifpe.projetoSelenium;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
